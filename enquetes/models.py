314e4422f3bdcd87c8ec6ff0695ab8321ac15d85
PhePheMath <PhelyppeMath@outlook.com>
2020-08-29 23:19:30 -0300

    Model enquetes

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Enquetes(models.Model):
+
+    nome = models.CharField(null=True, blank=True, max_length=80)
+    idade = models.IntegerField(null=True, blank=True)
+    enquete = models.TextField(null=True, blank=True)
