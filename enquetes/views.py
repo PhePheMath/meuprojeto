6585a8bb6f7ae4ba36d9f1b73d69c78ba92d5fa3
PhePheMath <PhelyppeMath@outlook.com>
2020-08-29 23:19:59 -0300

    Views to manage enquetes

--- enquetes/views.py
+++ enquetes/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from .models import Enquetes
+from .forms import EnqueteForm
 
-# Create your views here.
 def boasvindas(request):
     return render(request, 'bemvindo.html')
+
+def criar_enquete(request):
+    if request.method == 'POST':
+        form = EnqueteForm(request.POST)
+        if form.is_valid():
+            enquete = form.save()
+            return HttpResponseRedirect(reverse('mostrar_enquete', kwargs={'enquete_id': enquete.pk}))
+    return render(request, 'enquete_form.html', {'form': EnqueteForm()})
+
+def show_enquete(request, enquete_id):
+    enquete = Enquetes.objects.get(id=enquete_id)
+    return render(request, 'enquetes.html', {'enquete':enquete})
